a50b6363d96b6cbca57d38f39d8de1c3ad3319b0
multiplaie <multiplaie@protonmail.com>
2021-11-16 14:20:47 +0100

    add get one chapter by his id feature

--- src/routes/chapterController.js
+++ src/routes/chapterController.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 const {ChapterModel} = require('../models/chapterModel');
 
-router.get('/:story_id', (req, res) => {
+router.get('/allActiveByStory/:story_id', (req, res) => {
     ChapterModel.find({archive: null, story: req.params.story_id},(err, chapter) => {
         if(!err) res.send(chapter);
         else console.log('Error to get data:' + err);
     });
 });
+
+
+router.get('/oneById/:id', (req, res) => {
+    ChapterModel.findOne({ _id: req.params.id},(err, chapter) => {
+        if(!err) res.send(chapter);
+        else console.log('Error to get data:' + err);
+    });
+});
+
 
 router.post('/', (req, res) =>{
     console.log(req)
